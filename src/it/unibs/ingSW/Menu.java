d444ae4c636a6501824efcba8bfc0f87781c0f98
glisenti.mirko <glisenti.mirko@blninformatica.it>
2021-04-15 01:49:47 +0200

    first commit

---
+++ src/it/unibs/ingSW/Menu.java
@@ -0,0 +1,51 @@
+package it.unibs.ingSW;
+
+import it.unibs.fp.mylib.*;
+
+public class Menu 
+{
+	public static void main(String[] args) throws Exception{
+		int scelta = 0;
+		do 
+		{
+			System.out.println("Menu principale [utente: configuratore]\n1) Gestione reti N\n\n0) Esci");
+			scelta = InputDati.leggiIntero("Selezionare una delle voci del menu:", 0, 2);
+			switch (scelta)
+			{
+				case 1: 
+					System.out.println("\n");
+					reti();
+				break;
+				default:
+					System.out.println("Uscita in corso...");
+			}
+		} 
+		while (scelta != 0);
+	}
+	
+	public static void reti(){
+		int scelta = 0;
+		do 
+		{
+			System.out.println("Menu principale Reti N [utente: configuratore]\n1) Inserisci descrizione \n2) Visualizza descrizione \n\n0) Esci");
+			scelta = InputDati.leggiIntero("Selezionare una delle voci del menu:", 0, 2);
+			switch (scelta)
+			{
+				case 1: 
+					AggiungiN aggiungi = new AggiungiN();
+					aggiungi.add();
+					System.out.println("\n");
+				break;
+				case 2:
+					VisualizzaN visualizza = new VisualizzaN();
+					visualizza.print();
+					System.out.println("\n");
+				break;
+				default:
+					System.out.println("\n");
+				break;
+			}
+		} 
+		while (scelta != 0);
+	}
+}
